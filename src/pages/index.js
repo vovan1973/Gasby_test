a1f3daba68868359e5bbf0802bc81742cdd15396
Vovan1973 <teresvovan1973@gmail.com>
2025-05-25 00:52:22 +0300

    Закончили учебника Gastby часть-3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
       <Layout pageTitle="Home Page">
       <p>Главная страница </p>
+      <StaticImage
+      alt="Собака"
+       src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   )
 }
