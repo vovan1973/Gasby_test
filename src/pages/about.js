cb4f68f8da19e3b46314ea0b64f4b26f28b22b8b
Vovan1973 <teresvovan1973@gmail.com>
2025-05-24 23:44:04 +0300

    Закончили учебника Gastby часть-2

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+     <Layout pageTitle="About Me">
+      <p>Страница о нас</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+// Step 3: Export your component
+export default AboutPage
+
